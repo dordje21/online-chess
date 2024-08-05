6da3170141160f9cb5c1f30423bce22cc5d3534f
MykhailoLytvyn <cresedel211194@gmail.com>
2024-08-05 14:47:58 +0200

    fix click mobile

--- front/public/js/index.js
+++ front/public/js/index.js
@@ -1,58 +1,116 @@
 
-let gameHasStarted = false;
+let gameHasStarted = false
 var board = null
+// r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b - - 0 19
 var game = new Chess()
 var $status = $('#status')
 var $pgn = $('#pgn')
-let gameOver = false;
+let gameOver = false
 
-function onDragStart (source, piece, position, orientation) {
+let $board = $('#myBoard')
+let squareClass = 'square-55d63'
+let squareToHighlight = null
+let colorToHighlight = null
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
+
+function removeGreySquares() {
+    $('#myBoard .square-55d63').css('background', '')
+}
+
+function greySquare(square) {
+    var $square = $('#myBoard .square-' + square)
+
+    var background = whiteSquareGrey
+    if ($square.hasClass('black-3c85d')) {
+        background = blackSquareGrey
+    }
+
+    $square.css('background', background)
+}
+
+function onDragStart(source, piece, position, orientation) {
+
     // do not pick up pieces if the game is over
-    if (game.game_over()) return false
-    if (!gameHasStarted) return false;
-    if (gameOver) return false;
+    if (game.game_over()) {
+        console.log('// game.game_over')
+        return false
+    }
+    if (!gameHasStarted) {
+        console.log('//!gameHasStarted')
+        return false
+    }
+    if (gameOver) {
+        console.log('//gameOver')
+        return false
+    }
 
     if ((playerColor === 'black' && piece.search(/^w/) !== -1) || (playerColor === 'white' && piece.search(/^b/) !== -1)) {
-        return false;
+        console.log('// black only pick up pieces for the side to move')
+        return false
     }
 
     // only pick up pieces for the side to move
     if ((game.turn() === 'w' && piece.search(/^b/) !== -1) || (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        console.log('// wight only pick up pieces for the side to move')
         return false
     }
 }
 
-function onDrop (source, target) {
+function onDrop(source, target) {
     let theMove = {
         from: source,
         to: target,
         promotion: 'q' // NOTE: always promote to a queen for simplicity
-    };
+    }
     // see if the move is legal
-    var move = game.move(theMove);
+    var move = game.move(theMove)
 
 
     // illegal move
     if (move === null) return 'snapback'
 
-    socket.emit('move', theMove);
+    if (move.color === 'w') {
+        $board.find('.' + squareClass).removeClass('highlight-white')
+        console.log('move.from', move.from)
+        $board.find('.square-' + move.from).addClass('highlight-white')
+        squareToHighlight = move.to
+        colorToHighlight = 'white'
+    } else {
+        $board.find('.' + squareClass).removeClass('highlight-black')
+        $board.find('.square-' + move.from).addClass('highlight-black')
+        squareToHighlight = move.to
+        colorToHighlight = 'black'
+    }
+
+
+    socket.emit('move', theMove)
 
     updateStatus()
+
+    onMoveEnd()
 }
 
-socket.on('newMove', function(move) {
-    game.move(move);
-    board.position(game.fen());
-    updateStatus();
-});
+socket.on('newMove', function (move) {
+    game.move(move)
+    board.position(game.fen())
+    console.log('newMove', game.fen())
+    updateStatus()
+})
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
-function onSnapEnd () {
+function onSnapEnd() {
     board.position(game.fen())
+    console.log('onSnapEnd', game.fen())
 }
 
-function updateStatus () {
+function onMoveEnd() {
+    $board.find('.square-' + squareToHighlight)
+        .addClass('highlight-' + colorToHighlight)
+}
+
+function updateStatus() {
     var status = ''
 
     var moveColor = 'White'
@@ -86,13 +144,13 @@ function updateStatus () {
         if (game.in_check()) {
             status += ', ' + moveColor + ' is in check'
         }
-        
+
     }
 
     $status.html(status)
     $pgn.html(game.pgn())
 }
-
+// r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R
 var config = {
     draggable: true,
     position: 'start',
@@ -103,24 +161,24 @@ var config = {
 }
 board = Chessboard('myBoard', config)
 if (playerColor == 'black') {
-    board.flip();
+    board.flip()
 }
 
 updateStatus()
 
-var urlParams = new URLSearchParams(window.location.search);
+var urlParams = new URLSearchParams(window.location.search)
 if (urlParams.get('code')) {
     socket.emit('joinGame', {
         code: urlParams.get('code')
-    });
+    })
 }
 
-socket.on('startGame', function() {
-    gameHasStarted = true;
+socket.on('startGame', function () {
+    gameHasStarted = true
     updateStatus()
-});
+})
 
-socket.on('gameOverDisconnect', function() {
-    gameOver = true;
+socket.on('gameOverDisconnect', function () {
+    gameOver = true
     updateStatus()
-});
+})
